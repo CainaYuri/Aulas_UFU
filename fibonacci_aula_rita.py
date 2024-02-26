78c93b2349e53e5d4d0ce49b3a0eb0045d720a51
Yuri Caina <cainayuri@gmail.com>
2024-02-26 14:10:36 -0300

    Initial commit

---
+++ fibonacci_aula_rita.py
@@ -0,0 +1,14 @@
+def fibonacci(n):
+
+    if n<=1:
+
+        return 1git add hello.py
+
+        print("faça de novo")
+    elif n>1:
+
+        fib = fibonacci(n-1)+fibonacci(n-2)
+        return fib
+
+n= int(input("digite n:"))
+print(fibonacci(n))
